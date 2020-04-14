4453a453d5c6e3925d585388fcb0f2c5bfa58011
valcobe <valcobe95@hotmail.com>
2020-04-14 21:54:34 +0200

    APARTADO A hecho

--- app/src/main/java/com/android/mdw/demo/ElServicio.java
+++ app/src/main/java/com/android/mdw/demo/ElServicio.java
@@ -3,12 +3,14 @@ package com.android.mdw.demo;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.widget.Toast;
 
 public class ElServicio extends Service {
 
 	private MediaPlayer player;
+	private MediaPlayer player1;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -21,18 +23,29 @@ public class ElServicio extends Service {
 		Toast.makeText(this, R.string.creaserv, Toast.LENGTH_LONG).show();
 		player = MediaPlayer.create(this, R.raw.train);
 		player.setLooping(true);
+		player1 = MediaPlayer.create(this, R.raw.bob_marley_cybl);
+		player1.setLooping(true);
 	}
 
 	@Override
 	public void onDestroy() {
 		Toast.makeText(this, R.string.finaserv, Toast.LENGTH_LONG).show();
 		player.stop();
+		player1.stop();
 	}
 	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startid) {
 		Toast.makeText(this, R.string.iniserv, Toast.LENGTH_LONG).show();
-		player.start();
+		
+		Bundle data = intent.getExtras();
+		String so = data.getString(getString(R.string.mess));
+
+		if(so.equals(getString(R.string.selectSonido))) {
+			player.start();
+		}else if (so.equals(getString(R.string.selectCancion))){
+			player1.start();
+		}
 		return startid;		
 	}	
 
--- app/src/main/java/com/android/mdw/demo/Main.java
+++ app/src/main/java/com/android/mdw/demo/Main.java
@@ -6,33 +6,50 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Main extends Activity implements OnClickListener {
-  private Intent in;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
 
-    Button btnInicio = (Button) findViewById(R.id.btnInicio);
-    Button btnFin = (Button) findViewById(R.id.btnFin);
+    Button btnInicio = findViewById(R.id.btnInicio);
+    Button btnCancion = findViewById(R.id.btnCancion);
+    Button btnFin = findViewById(R.id.btnFin);
 
     btnInicio.setOnClickListener(this);
+    btnCancion.setOnClickListener(this);
     btnFin.setOnClickListener(this);
 
-    in = new Intent(this, ElServicio.class);
+
   }
 
   @Override
   public void onClick(View src) {
+    Intent in;
+
+    //APARTADO A
     switch (src.getId()) {
-    case R.id.btnInicio:      
+    case R.id.btnInicio:
+      Toast.makeText(this, R.string.select, Toast.LENGTH_LONG).show();
+      in = new Intent(this, ElServicio.class);
+      in.putExtra(getString(R.string.mess), getString(R.string.selectSonido));
       startService(in);
       break;
-    case R.id.btnFin:
-      stopService(in);
-      break;
+
+     case R.id.btnCancion:
+       Toast.makeText(this, R.string.select2, Toast.LENGTH_LONG).show();
+       in = new Intent(this, ElServicio.class);
+       in.putExtra(getString(R.string.mess), getString(R.string.selectCancion));
+       startService(in);
+       break;
+
+      case R.id.btnFin:
+       Toast.makeText(this, R.string.select3, Toast.LENGTH_LONG).show();
+       stopService(new Intent(this, ElServicio.class));
+       break;
     }
   }
 }
