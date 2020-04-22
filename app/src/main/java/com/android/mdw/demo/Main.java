b0b1178bb4192adb3d180fd56baf2c8a1390389a
valcobe <valcobe95@hotmail.com>
2020-04-22 11:55:46 +0200

    cambios en build.gradle y miniact terminada

--- app/src/main/java/com/android/mdw/demo/Main.java
+++ app/src/main/java/com/android/mdw/demo/Main.java
@@ -2,22 +2,21 @@ package com.android.mdw.demo;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.ActivityOptions;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.telephony.TelephonyManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
-import org.jetbrains.annotations.Nullable;
+
 
 public class Main extends Activity implements OnClickListener {
     ElReceptor receiver;
