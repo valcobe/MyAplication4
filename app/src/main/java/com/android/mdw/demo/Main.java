9671c873b5615a9bb9729a2f44856d9296da24cf
valcobe <valcobe95@hotmail.com>
2020-05-29 11:39:11 +0200

    ...

--- app/src/main/java/com/android/mdw/demo/Main.java
+++ app/src/main/java/com/android/mdw/demo/Main.java
@@ -53,44 +53,44 @@ public class Main extends Activity implements OnClickListener {
         switch (src.getId()) {
             case R.id.btnInicio:
 
-                Toast.makeText(this, R.string.select, Toast.LENGTH_LONG).show();
-                in = new Intent(this, ElServicio.class);
-                in.putExtra(getString(R.string.mess), getString(R.string.selectSonido));
-                startService(in);
-                break;
+                //Toast.makeText(this, R.string.select, Toast.LENGTH_LONG).show();
+                //in = new Intent(this, ElServicio.class);
+                //in.putExtra(getString(R.string.mess), getString(R.string.selectSonido));
+                //startService(in);
+                //break;
 
-//                Toast.makeText(this, R.string.select, Toast.LENGTH_LONG).show();
-//                in = new Intent(this, ElReceptor.class);
-//                in.putExtra(getString(R.string.mess), getString(R.string.selectSonido));
-//                sendBroadcast(in);
-//                break;
+                Toast.makeText(this, R.string.select, Toast.LENGTH_LONG).show();
+                in = new Intent(this, ElReceptor.class);
+                in.putExtra(getString(R.string.mess), getString(R.string.selectSonido));
+                sendBroadcast(in);
+                break;
 
             case R.id.btnCancion:
 
-                Toast.makeText(this, R.string.select2, Toast.LENGTH_LONG).show();
-                in = new Intent(this, ElServicio.class);
-                in.putExtra(getString(R.string.mess), getString(R.string.selectCancion));
-                startService(in);
-                break;
+                //Toast.makeText(this, R.string.select2, Toast.LENGTH_LONG).show();
+                //in = new Intent(this, ElServicio.class);
+                //in.putExtra(getString(R.string.mess), getString(R.string.selectCancion));
+                //startService(in);
+                //break;
 
-//                Toast.makeText(this, R.string.select2, Toast.LENGTH_LONG).show();
-//                in = new Intent(this, ElReceptor.class);
-//                in.putExtra(getString(R.string.mess), getString(R.string.selectCancion));
-//                sendBroadcast(in);
-//                break;
+                Toast.makeText(this, R.string.select2, Toast.LENGTH_LONG).show();
+                in = new Intent(this, ElReceptor.class);
+                in.putExtra(getString(R.string.mess), getString(R.string.selectCancion));
+                sendBroadcast(in);
+                break;
 
             case R.id.btnFin:
 
+                //Toast.makeText(this, R.string.select3, Toast.LENGTH_LONG).show();
+                //stopService(new Intent(this, ElServicio.class));
+                //break;
+
+
                 Toast.makeText(this, R.string.select3, Toast.LENGTH_LONG).show();
-                stopService(new Intent(this, ElServicio.class));
+                in = new Intent(this, ElReceptor.class);
+                in.putExtra(getString(R.string.mess), getString(R.string.selectDetencion));
+                sendBroadcast(in);
                 break;
-
-
-//                Toast.makeText(this, R.string.select3, Toast.LENGTH_LONG).show();
-//                in = new Intent(this, ElReceptor.class);
-//                in.putExtra(getString(R.string.mess), getString(R.string.selectDetencion));
-//                sendBroadcast(in);
-//                break;
 
             //Optatiu D
             case R.id.button1:
